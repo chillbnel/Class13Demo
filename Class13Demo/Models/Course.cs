f78c0dc244fa707946e6c45f6dc10c4da0af19b0
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-10-24 16:55:27 -0700

    Hour 2

---
+++ Class13Demo/Models/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class13Demo.Models
+{
+    public class Course
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public Technology Technology { get; set; }
+        public double Price { get; set; }
+
+        public ICollection<CourseEnrollments> Enrollments { get; set; }
+        public ICollection<Transcript> Transcripts { get; set; }
+    }
+
+    public enum Technology
+    {
+        dotnet,
+        python,
+        java
+    }
+}
---
+++ Class13Demo/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class13Demo.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }//assigns a key
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+
+        public ICollection<CourseEnrollments> Courses { get; set; }
+        public ICollection<Transcript> Transcripts { get; set; }
+    }
+
+
+}
